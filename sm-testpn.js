7b5d40f762807f8983a035eaf667a66ec84ddea0
Patrick Nelson <nelson.patrick@con-way.com>
2010-07-21 10:42:33 -0700

    update testpn v34

--- sm-testpn.js
+++ sm-testpn.js
@@ -6,35 +6,18 @@ print("=====================");
 // Test Variable Assignment
 //--------------------------
 
-print("-----------------");
+aCnt = 0;
 
-aDT      = new Array("56k", "800 dedicated", "800 switched", "atm", "bill number only", "broadband", "centrex", "coin", "dial", "did extension", "did trunk", "dsl", "e1", "frame relay", "icn", "isdn", "isdn bri", "isdn pri", "leased line", "local", "local gs trunk", "local ls line", "metro-ethernet", "mpls", "multi-pt", "oc12", "oc3", "primepath", "pt-pt", "pvc meet", "rcf", "t1", "t1 - isdn ld", "t1 - isdn local", "t1 - ld", "t1 - local", "t3", "vpn-b2b");
-fDevType = new SCFile("devtype");
-fDevType.doSelect("device.name = \"line\"");
+print("aCnt Before -> " + aCnt);
 
-print( "device.name: \n" + fDevType.sub_type);
-print( "length     : \n" + fDevType.sub_type.length());
+aCnt += 1;
 
-for ( i=0 ; i<fDevType.sub_type.length() ; i++) {
-  fDevType.sub_type.pop();
-  print("popping");
-}
+print("aCnt After -> " + aCnt);
 
 
-print( "device.name: \n" + fDevType.sub_type);
-
-fDevType.doUpdate();
-
-print( "device.name: \n" + fDevType.sub_type);
-
-for ( i=0 ; i<aDT.length ; i++) {
-  fDevType.sub_type.push(aDT[i]);
-  print("pushing -> "+aDT[i]);
-}
-
-fDevType.doUpdate();
-
-print( "device.name: \n" + fDevType.sub_type);
+  var cnwChangeCICount = webserviceArrayCount(sm_rfc.get("middle.assets.assets"));
+  generic_rfc.setField("cnw-change-ci", cnwChangeCICount);
+  logger.debug(myId + " - convertChange - setting cnw-change-ci to " + cnwChangeCICount);
 
 print("=====================");
 print("Done");
