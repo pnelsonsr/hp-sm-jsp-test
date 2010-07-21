81c6b2ec699dd66d873e00155b2f42a50f452e8f
Patrick Nelson <nelson.patrick@con-way.com>
2010-07-21 10:21:03 -0700

    update testpn v05

--- sm-testpn.js
+++ sm-testpn.js
@@ -3,32 +3,19 @@ print("Start");
 print("=====================");
 
 var fChange = new SCFile( "cm3r" );
-var aCI     = new Array();
-var aAG     = new Array();
-var iIdx    = 0;
-var cSql    = "number = \"CM0000267\"";
-var gRC     = fChange.doSelect( cSql );
+var aAG= new Array();
+var sql = "number = \"CM0000267\"";
+var rc = fChange.doSelect( sql );
+
 print("result 1a -> " + fChange);
 print("result 1b -> " + aAG.length);
 print("=====================");
 
-aCI.push(system.library.cnfcm.GetCI(fChange));
-if(aCI.length = 1) {
-  aAG.push(system.library.cnfcm.GetAG(aCI));
-} else if (aCI.length > 1) {
-  for( i=0 ; i<aCI.length() ; i++ ) {
-    print("result 3c" + i + " -> " + aCI[i]);
-  }
-}
-
-
-
-
 if(aAG.length > 0) {
- aAG.push(system.library.cnfcm.GetAG(fChange));
+ aAG.push(system.library.cnfcm.GetOpAG(fChange));
  print("result 2a -> " + aAG);
 } else {
- aAG.push(system.library.cnfcm.GetAG(fChange));
+ aAG.push(system.library.cnfcm.GetOpAG(fChange));
  print("result 2a -> " + aAG);
 }
 
