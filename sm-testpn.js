a7fdda3d40e6bf9c01a7be2ce1456bfce93ed80a
Patrick Nelson <nelson.patrick@con-way.com>
2010-07-21 10:23:42 -0700

    update testpn v09

--- sm-testpn.js
+++ sm-testpn.js
@@ -5,27 +5,15 @@ print("=====================");
 //--------------------------
 // Test Variable Assignment
 //--------------------------
-//cRFC = "CM0000268"; /* zero CIs       */
-  cRFC = "CM0000267"; /* one CI         */
-//cRFC = "CM0000266"; /* two CIs        */
-//cRFC = "RC9999999"; /* Bad RFC Number */
-
-  var fAssignment = new SCFile( "assignment" );
-  print("result 1 -> " + fAssignement);
-  sql = "contact.name = \"" + fChange.orig_operator +"\"";
-  var rc = fOperator.doSelect( sql );
-  if(rc == RC_SUCCESS) {
-    print("result 3b -> " + fOperator.name);
-    print("result 3c -> " + fOperator.profile_change);
-    for( i=0 ; i<fOperator.profile_change.length() ; i++ ) {
-      if(fOperator.profile_change[i] != null) {
-        print("result 3d"+i+" -> " + fOperator.profile_change[i]);
-      }
-    }
+  sAG="TEST - Storage AG";
+//sAG="";
+  sAGTL=system.library.cnfcm.GetAGLead(sAG);
+  if(sAGTL!=null) {
+    print("TeamLead -> "+sAGTL);
   } else {
-    print("Failed")
+    print("TeamLead -> NONE");
   }
-
+  system.library.cnfcm.SayIt("Hello");
 
 
 print("=====================");
