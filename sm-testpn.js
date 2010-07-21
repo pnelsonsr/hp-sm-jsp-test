bbde04461327da9beb3aaee161019239b938a646
Patrick Nelson <nelson.patrick@con-way.com>
2010-07-21 10:22:58 -0700

    update testpn v08

--- sm-testpn.js
+++ sm-testpn.js
@@ -5,18 +5,28 @@ print("=====================");
 //--------------------------
 // Test Variable Assignment
 //--------------------------
-//cRFC = "CM0000070"; /* zero CIs       */
-  cRFC = "CM0000003"; /* one CI         */
-//cRFC = "CM0000069"; /* two CIs        */
+//cRFC = "CM0000268"; /* zero CIs       */
+  cRFC = "CM0000267"; /* one CI         */
+//cRFC = "CM0000266"; /* two CIs        */
 //cRFC = "RC9999999"; /* Bad RFC Number */
 
-//  bClear=true;
-    bClear=false;
+  var fAssignment = new SCFile( "assignment" );
+  print("result 1 -> " + fAssignement);
+  sql = "contact.name = \"" + fChange.orig_operator +"\"";
+  var rc = fOperator.doSelect( sql );
+  if(rc == RC_SUCCESS) {
+    print("result 3b -> " + fOperator.name);
+    print("result 3c -> " + fOperator.profile_change);
+    for( i=0 ; i<fOperator.profile_change.length() ; i++ ) {
+      if(fOperator.profile_change[i] != null) {
+        print("result 3d"+i+" -> " + fOperator.profile_change[i]);
+      }
+    }
+  } else {
+    print("Failed")
+  }
 
-    bShow=true;
-//  bShow=false;
 
-system.library.cnfcm.GetImpact(cRFC,bClear,bShow)
 
 print("=====================");
 print("Done");
