bed743e0e34c19355f2b70d49d5a1d4add5b83c1
Patrick Nelson <nelson.patrick@con-way.com>
2010-07-21 10:38:55 -0700

    update testpn v27

--- sm-testpn.js
+++ sm-testpn.js
@@ -8,18 +8,10 @@ print("=====================");
 
 var sAG="IT Client Systems Infrastructure";
 
-var fIAG = new SCFile("assignment")
-var getAssignment = fIAG.doSelect("name#\""+sAG+"\"")
-if (getAssignment == RC_SUCCESS) {
-  var operatorList = fIAG.operators
-  var operatorList=operatorList.getText()
-  var sAGCList="operator.id isin "+operatorList
-}
-print("sAGCList -> "+sAGCList);
+sTL = system.library.cnfcm.GetAGLead(sAG);
 
-// IMEDIATE IF imediate if IMMEDIATE IF immediate if IIF iif
+print("sTL -> "+sTL);
 
-var myVAR=(true) ? "true" : "false";
 
 print("=====================");
 print("Done");
