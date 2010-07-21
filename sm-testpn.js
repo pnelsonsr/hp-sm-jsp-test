ad771a44c997d2e08c05bbff0365fdcb98c8007e
Patrick Nelson <nelson.patrick@con-way.com>
2010-07-21 10:50:43 -0700

    update testpn v48

--- sm-testpn.js
+++ sm-testpn.js
@@ -2,13 +2,22 @@ print("=====================");
 print("Start");
 print("=====================");
 
-var sTheRFC = "CM0000104";
-//system.library.cnfcm.SetLateRFC({acRFC:sTheRFC,acDoUpdate:true,acShowIt:false});
-system.library.cnfcm.SetLateRFC({acRFC:sTheRFC});
+  var nCnt = 0;
+  var fRFC = new SCFile("cm3r");
+  var rRC  = fRFC.doSelect(new QueryCond("cnf.planned.days", EQ, null)); 
+  while (rRC==RC_SUCCESS) {
+    fRFC.cnf_planned_days = "0";
+    print(fRFC.number+" -> updated - "+fRFC.cnf_planned_days);
+    //fRFC.doUpdate();
+    nCnt += 1;
+    rRC = fRFC.getNext();
+  }
+  print("total null records -> "+nCnt);
 
 print("=====================");
 print("Done");
 print("=====================");
+
 
 /*
 //-----------------------
