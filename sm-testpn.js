29075acdc051de35971f5279a4c8c642b91d4774
Patrick Nelson <nelson.patrick@con-way.com>
2010-07-21 10:44:08 -0700

    update testpn v37

--- sm-testpn.js
+++ sm-testpn.js
@@ -2,38 +2,31 @@ print("=====================");
 print("Start");
 print("=====================");
 
-var aCIUS = new Array();
+//--------------------------
+// Test Variable Assignment
+//--------------------------
 
-iRI=0 ; iLI=1 ; iLvl=1; 
+print("-----------------");
 
-aCIUS[iRI]=[iLvl];
-print("aCIUS -> " + aCIUS);
-aCIUS[iRI][iLI]="Test Sys 1";
-print("aCIUS -> " + aCIUS);
-
-iRI=1 ; iLI=1 ; iLvl=2; 
-
-aCIUS[iRI]=[iLvl];
-print("aCIUS -> " + aCIUS);
-aCIUS[iRI][iLI]="Test Sys 2";
-print("aCIUS -> " + aCIUS);
-
-print("aCIUS.length -> " + aCIUS.length);
-
-print("==================");
-
-iRI=0;
-for(iLI=0 ; iLI<aCIUS.length ; iLI++) {
-  print("aCIUS[" + iRI + "][" + iLI + "] -> " + aCIUS[iRI][iLI]);
+if (!system.functions.gui()) {
+  if (record.approval_status="approved") {
+    var getchange = new SCFile("cm3r");
+    var query ="number=\""+record.unique_key+"\"";
+    var rc = getchange.doSelect(query);
+    if (rc==RC_SUCCESS) {
+      if (getchange.current_phase== "Pending Acceptance" || getchange.current_phase== "Pending Approval") {
+        getchange.doAction("close");
+      } else {
+        print("DEBUG: Incorrect Phase")
+      }
+    } else {
+      print("ERROR: No Change for Approval found")
+    }
+  }
 }
 
-print("==================");
-
-iLI=1;
-for(iRI=0 ; iRI<aCIUS.length ; iRI++) {
-  print("aCIUS[" + iRI + "][" + iLI + "] -> " + aCIUS[iRI][iLI]);
-}
-
+print("-----------------");
+ 
 
 print("=====================");
 print("Done");
