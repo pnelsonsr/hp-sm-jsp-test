f6e0629c2b6426e85a1409aab6b71fbea77d4b3a
Patrick Nelson <nelson.patrick@con-way.com>
2010-07-21 10:34:53 -0700

    update testpn v19

--- sm-testpn.js
+++ sm-testpn.js
@@ -2,32 +2,26 @@ print("=====================");
 print("Start");
 print("=====================");
 
-  cRFC = "CM0000026"; /* one CI*/
-  fChange = new SCFile("cm3r");
-  aCI = new Array() ; aCIUS = new Array() 
-  aCI.push("IT DATABASE FINANCIALS","IT Client Systems Infrastructure","IT DATABASE DB2");
+fAD = new SCFile("approvaldef");
+rRC=fAD.doSelect(new QueryCond("name", EQ, "Approval Normal")); 
 
-print("aCI -> "+aCI);
-print("Length -> "+aCI.length);
+print("name -> "+fAD.group_name);
+print("1 length -> "+fAD.group_name.length());
 
-if(fChange.doSelect("number = \"" + cRFC + "\"")==RC_SUCCESS) {
- print("Hey I Found it -> "+fChange.cnf_approval_list);
+iCnt=fAD.group_name.length();
+bUpdate=false;
+
+for(i=0 ; i<iCnt ; i++) {
+  bUpdate=true;
+  fAD.group_name.pop();
+  fAD.sequence.pop();
+  fAD.condition.pop();
 }
 
-for(i=0 ; i<aCI.length ; i++) {
-  if(i==0 && fChange.cnf_approval_list.length()==1) {
-    fChange.cnf_approval_list[0]=aCI[i];
-    print("add -> "+aCI[i]);
-  } else {
-    fChange.cnf_approval_list.push(aCI[i]);
-    print("push -> "+aCI[i]);
-  }
-} 
+print("name -> "+fAD.group_name);
+print("2 length -> "+fAD.group_name.length());
 
-print("New Value -> "+fChange.cnf_approval_list);
-print("Length -> "+fChange.cnf_approval_list.length());
-
-fChange.doUpdate();
+if(bUpdate) {fAD.doUpdate();}
 
 print("=====================");
 print("Done");
