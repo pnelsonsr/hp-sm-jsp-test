2a06ba4a3747c1c78c9b65d362ee7fefb7c98a76
Patrick Nelson <nelson.patrick@con-way.com>
2010-07-21 10:31:50 -0700

    update testpn v14

--- sm-testpn.js
+++ sm-testpn.js
@@ -2,18 +2,20 @@ print("=====================");
 print("Start");
 print("=====================");
 
-//--------------------------
-// Test Variable Assignment
-//--------------------------
+var d1 = new Date(); 
+var d2 = new Date();
 
-sSayWhat="This and That";
-//iSayHow=3;
-//system.library.cnfcm.SayItNow(sSayWhat,iSayHow);
-system.library.cnfcm.SayIt(sSayWhat);
+d1="03/23/09 13:22:11";
+d2="03/24/09 10:20:10";
 
+print("d1 -> " + d1);
+print("d2 -> " + d2);
+
+print("=====================");
+
+result = system.library.cnfcm.Duration(d1,d2);
+print("result 1 -> " + result);
 
 print("=====================");
 print("Done");
 print("=====================");
-
-//print("-----------------");
