92eadc4ed6c86122064aec7d7ae3b3d4da1955e7
Patrick Nelson <nelson.patrick@con-way.com>
2010-07-21 10:41:24 -0700

    update testpn v32

--- sm-testpn.js
+++ sm-testpn.js
@@ -8,6 +8,7 @@ print("=====================");
 
 print("-----------------");
 
+aDT      = new Array("56k", "800 dedicated", "800 switched", "atm", "bill number only", "broadband", "centrex", "coin", "dial", "did extension", "did trunk", "dsl", "e1", "frame relay", "icn", "isdn", "isdn bri", "isdn pri", "leased line", "local", "local gs trunk", "local ls line", "metro-ethernet", "mpls", "multi-pt", "oc12", "oc3", "primepath", "pt-pt", "pvc meet", "rcf", "t1", "t1 - isdn ld", "t1 - isdn local", "t1 - ld", "t1 - local", "t3", "vpn-b2b");
 fDevType = new SCFile("devtype");
 fDevType.doSelect("device.name = \"line\"");
 
@@ -19,7 +20,7 @@ for ( i=0 ; i<fDevType.sub_type.length() ; i++) {
   print("popping");
 }
 
-var aDT = new Array("56k", "800 dedicated", "800 switched", "atm", "bill number only", "broadband","centrex", "coin", "dial", "did", "did extension", "did trunk", "dsl", "e1", "frame relay", "icn", "isdn", "isdn bri", "isdn pri", "leased line", "local", "local gs trunk", "local ls line", "metro-ethernet", "mpls", "multi-pt", "oc12", "oc3", "primepath", "pt-pt", "pvc meet", "rcf", "t1", "t1 - isdn ld", "t1 - isdn local", "t1 - ld", "t1 - local", "t3", "vpn-b2b");
+print( "device.name: \n" + fDevType.sub_type);
 
 fDevType.doUpdate();
 
