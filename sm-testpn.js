9734e170a72f885f82dc5ac250a672d322592dee
Patrick Nelson <nelson.patrick@con-way.com>
2010-07-21 10:35:57 -0700

    update testpn v21

--- sm-testpn.js
+++ sm-testpn.js
@@ -2,40 +2,12 @@ print("=====================");
 print("Start");
 print("=====================");
 
-var fAD = new SCFile("approvaldef");
-var fAG = new SCFile("assignment");
-var aAL = new Array();
-var rRC;
-rRC=fAG.doSelect(new QueryCond("name", NEQ, "")); 
-while (rRC == RC_SUCCESS) 	{
-  print("-> "+fAG.name);
-  if(aAL.length==0) {aAL[0]=fAG.name;} else {aAL.push(fAG.name);}
-  rRC = fAG.getNext();
-}
-rRC=fAD.doSelect(new QueryCond("name", EQ, "Approval Normal")); 
-bUpdate=false;
-for(i=0 ; i<aAL.length ; i++) {
-  if(system.library.cnfcm.IsUnique(fAD.group_name,aAL[i])) { 
-    bUpdate=true;
-    if(i==0 && fAD.group_name[0]==null) {
-      fAD.group_name[0]=aAL[i];
-      fAD.sequence[0]=1;
-      fAD.condition[0]="index(\""+aAL[i]+"\",cnf.approval.list in $L.file)>0";
-      print("added ->"+aAL[i]);
-    } else {
-      fAD.group_name.push(aAL[i]);
-      fAD.sequence.push(1);
-      fAD.condition.push("index(\""+aAL[i]+"\",cnf.approval.list in $L.file)>0");
-      print("pushed ->"+aAL[i]);
-    }
-  
-  } else {
-    print("AG in AD already -> "+aAL[i]);
-  }
-}
-fAD.doUpdate();  
-system.functions.cleanup(fAD);
+  var sAD="Approval Normal";
+//var sAD="Approval Standard";
+//var sAD="Approval Emergency";
 
+ClearAD(sAD);
+PopulateAD(sAD);
 
 print("=====================");
 print("Done");
