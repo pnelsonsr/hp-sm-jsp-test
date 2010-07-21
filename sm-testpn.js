7d6010fd74541c80cdbe3d1d0017854c776593d1
Patrick Nelson <nelson.patrick@con-way.com>
2010-07-21 10:24:27 -0700

    update testpn v10

--- sm-testpn.js
+++ sm-testpn.js
@@ -5,15 +5,57 @@ print("=====================");
 //--------------------------
 // Test Variable Assignment
 //--------------------------
-  sAG="TEST - Storage AG";
-//sAG="";
-  sAGTL=system.library.cnfcm.GetAGLead(sAG);
-  if(sAGTL!=null) {
-    print("TeamLead -> "+sAGTL);
+//sCI="AADADB";                //Applications and Software
+//sCI="Net Backup Server 2";   //computer
+//sCI="BMCPDS";                //database
+//sCI="bipdp2.con-way.com";    //networkcomponents
+  sCI="Big Hard Disk";         //storage
+//sCI="";
+
+  sOwningTeam = system.library.cnfcm.GetAGFromCI(sCI);
+  if(sOwningTeam==null | sOwningTeam=="AUTO") {
+    sOwningTeam = ""
+    sOwner = "hamilton, jim";
   } else {
-    print("TeamLead -> NONE");
+    sOwner = system.library.cnfcm.GetAGLead(sOwningTeam);
   }
-  system.library.cnfcm.SayIt("Hello");
+  sAssignedTeam=sOwningTeam;
+  if(sOwner==null) {sOwner = "hamilton, jim";}
+  sAssignedTo=sOwner;
+  sChangeManager=sOwner
+  
+  print("sOwningTeam -> "+sOwningTeam);
+  print("sOwner -> "+sOwner);
+  print("sAssignedTeam -> "+sAssignedTeam);
+  print("sAssignedTo -> "+sAssignedTo);
+
+  sCIType=system.library.cnfcm.GetCIType(sCI);
+  if(sCIType!=null) {
+    print("Type -> "+sCIType);
+  } else {
+    print("Type -> NONE");
+  }
+  
+  if(sCIType=="Applications and Software") {
+    sService="Applications";
+  } else if(sCIType=="computer") {
+    sService="Hardware";
+  } else if(sCIType=="database") {
+    sService="Database";
+  } else if(sCIType=="networkcomponents") {
+    sService="Network";
+  } else if(sCIType=="storage") {
+    sService="Storage";
+  } else {
+    sService="";
+  }  
+
+  if(sService!=null) {
+    print("Service -> "+sService);
+  } else {
+    print("Service -> NONE");
+  }
+
 
 
 print("=====================");
