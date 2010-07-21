16d27b07c7fcab589b2a53374d6ae547e3058c0d
Patrick Nelson <nelson.patrick@con-way.com>
2010-07-21 10:51:35 -0700

    update testpn v50

--- sm-testpn.js
+++ sm-testpn.js
@@ -2,28 +2,17 @@ print("=====================");
 print("Start");
 print("=====================");
 
-
-nLate = 3;nLateCnt = 0;nOpenCnt = 0;
-bLate = false;
-dLate = new Date();
-dLate.setDate(dLate.getDate()-nLate)
-print("Date Late -> "+dLate);
-fCM3R = new SCFile("cm3r");
-rRC=fCM3R.doSelect("current.phase<>\"Closed\""); 
-while (rRC == RC_SUCCESS) 	{
-  nOpenCnt +=1;
-  //bLate = (fCM3R.current_phase!="New" && fCM3R.planned_end!=null && parseInt((dLate-fCM3R.planned_end)/86400000)>0) ? true:false;
-  bLate = (fCM3R.current_phase=="New" && fCM3R.planned_end!=null && parseInt((dLate-fCM3R.planned_end)/86400000)>0) ? true:false;
-  //bLate = (fCM3R.planned_end!=null && parseInt((dLate-fCM3R.planned_end)/86400000)>0) ? true:false;
-  if (bLate) {
-    nLateCnt += 1;
-    print("Open -> "+fCM3R.number+" - "+fCM3R.current_phase);
-    print("  - Overdue - "+fCM3R.planned_end);
+  var nCnt = 0;
+  var fRFC = new SCFile("cm3r");
+  var rRC  = fRFC.doSelect(new QueryCond("cnf.planned.days", EQ, null)); 
+  while (rRC==RC_SUCCESS) {
+    fRFC.cnf_planned_days = "0";
+    print(fRFC.number+" -> updated - "+fRFC.cnf_planned_days);
+    //fRFC.doUpdate();
+    nCnt += 1;
+    rRC = fRFC.getNext();
   }
-  rRC = fCM3R.getNext();
-}
-print("Open RFCs -> "+nOpenCnt);
-print("Late RFCs -> "+nLateCnt);
+  print("total null records -> "+nCnt);
 
 print("=====================");
 print("Done");
