fba37c311f6912eccfb30dc0cb7636fd01d8175c
Patrick Nelson <nelson.patrick@con-way.com>
2010-07-30 18:51:27 -0700

    update sm-testpn v61

--- sm-testpn.js
+++ sm-testpn.js
@@ -2,37 +2,55 @@ print("=====================");
 print("Start");
 print("=====================");
 
-sFullRFC="CM0001234";
-//sFullRFC="1234";
-
-print("1-> "+sFullRFC);
-sDPadRFC=DPadRFCNum({acRFC:sFullRFC})
-print("2-> "+sDPadRFC);
-
-
-//-----------------------------------------------
-function DPadRFCNum(aoObj) {
-//-----------------------------------------------
-var oArgs=DArg({acRFC:0,acSA:false},aoObj);if(oArgs.acSA){SArg(oArgs);}
-//-----------------------------------------------
-// abRFC : false  -> RFC number
-// abSA  : false  -> Show function call Arguments 
-//-----------------------------------------------
-// RETURNS the RFC correctly padded
-//-----------------------------------------------
-  var sDPad;var sRFC;var bZeros=true;
-  if(oArgs.acRFC.toString().substr(0,2)!="CM"){return oArgs.acRFC;}
-  sRFC=oArgs.acRFC.toString().substr(2,7);
-  while(bZeros){
-    if(sRFC.substr(0,1)=="0"){sRFC=sRFC.substr(1);} else {bZeros=false;}
-  }
-  return sRFC;
-}
-
+var sNot="Not ";var sBy=" by -> ";var sBlk="";var bLck;var sOwner;var sSay;
+var sRFC="CM0000104";
+bLck = GetRFCLock({acRFC:sRFC,abRN:true,anShw:1})
+print(((!bLck)?sNot:sBlk)+"Locked"+((bLck)?sBy:sBlk)+((bLck)?bLck:sBlk));
 
 print("=====================");
 print("Done");
 print("=====================");
+
+
+//---------------------------------------------------------------------------------
+function GetRFCLock(aoObj) {
+//---------------------------------------------------------------------------------
+var oA=DArg({acRFC:0,abRN:false,anShw:0,acSA:false},aoObj);if(oA.acSA){SArg(oA);}
+//---------------------------------------------------------------------------------
+// abRFC : false  - RFC number
+// abRN  : false  - Return name of locking user
+// anShw : 0      - Level of show process info 
+// abSA  : false  - Show function call Arguments 
+//---------------------------------------------------------------------------------
+// RETURNS the RFC Lock State
+//---------------------------------------------------------------------------------
+  var sName=system.functions.get_lock_owner("cm3r;"+oA.acRFC);
+  if(sName==null){if(oA.anShw>=1){print("#1");}return false;}
+  if(oA.abRN) {if(oA.anShw>=1){print("#2");}return sName;} 
+  if(oA.anShw>=1){print("#3");}return true;
+}
+
+//---------------------------------------------------------------------------------
+function SArg(oArgs) {
+//---------------------------------------------------------------------------------
+// Prints out Args objects members
+//---------------------------------------------------------------------------------
+  print("SArg begin");
+  for(cArg in oArgs){print(" -"+cArg+" -> "+oArgs[cArg]);}
+  print("SArg end");
+}
+
+
+//---------------------------------------------------------------------------------
+function DArg(outArgs,inArgs) {
+//---------------------------------------------------------------------------------
+// RETURNS Args object updated with inArgs
+//---------------------------------------------------------------------------------
+  for(cArgNam in inArgs){outArgs[cArgNam]=inArgs[cArgNam];}
+  return outArgs;
+}
+
+
 
 /*
 //-----------------------
@@ -42,13 +60,3 @@ print("=====================");
 
 
 */
-
-//-----------------------------------------------
-function DArg(outArgs,inArgs) {
-//-----------------------------------------------
-// RETURNS Args object updated with inArgs
-//-----------------------------------------------
-  for (cArgNam in inArgs) {outArgs[cArgNam] = inArgs[cArgNam];}
-  return outArgs;
-}
-
